6ff51378c1290d05159f32044cd1cd4b27ab0a86
xu-shaoyong <15155459@qq.com>
2016-11-15 13:15:32 +0800

    完成遍历省市县三级列表的功能

--- src/model/CoolWeatherDB.java
+++ src/model/CoolWeatherDB.java
@@ -102,4 +102,37 @@ public class CoolWeatherDB {
 		}
 		return list;
 	}
+	/**
+	* 将County实例存储到数据库。
+	*/
+	public void saveCounty(County county) {
+		if (county != null) {
+			ContentValues values = new ContentValues();
+			values.put("county_name", county.getCountyName());
+			values.put("county_code", county.getCountyCode());
+			values.put("city_id", county.getCityId());
+			db.insert("County", null, values);
+		}
+	}
+	/**
+	* 从数据库读取某城市下所有的县信息。
+	*/
+	public List<County> loadCounties(int cityId) {
+		List<County> list = new ArrayList<County>();
+		Cursor cursor = db.query("County", null, "city_id = ?",
+				new String[] { String.valueOf(cityId) }, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				County county = new County();
+				county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				county.setCountyName(cursor.getString(cursor
+						.getColumnIndex("county_name")));
+				county.setCountyCode(cursor.getString(cursor
+						.getColumnIndex("county_code")));
+				county.setCityId(cityId);
+				list.add(county);
+			} while (cursor.moveToNext());
+		}
+		return list;
+	}
 }
